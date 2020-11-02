b8e11bbc22623276513464da31637aa3abe9b235
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-02 11:23:18 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
   let joke;
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => {
+      joke = jokeData.joke
+    })
   }
 
   form.addEventListener('submit', (event) => {
-
+    event.preventDefault()
+    const username = document.getElementById('name-input').value
+    
+    //guard clause
     if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+
+    fetchJoke() // fetchJoke is an asyncronous function that is pushed off the stack.
+      // .then runs once fetch joke is finished running
+      .then(() => {
+        const newJokeLi = document.createElement('li')
+        newJokeLi.innerHTML = `
+        <span class="username">${username} says:</span> ${joke}
+        `
+        jokeList.appendChild(newJokeLi)
+      })
   })
 })
